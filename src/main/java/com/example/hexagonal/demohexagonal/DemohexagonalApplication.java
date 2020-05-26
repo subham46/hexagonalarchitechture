b5ddd63348581bf5adecec5ce63f1374cd513add
subham46 <itshub46>
2020-05-26 15:08:11 +0530

    New hexagonal project

---
+++ src/main/java/com/example/hexagonal/demohexagonal/DemohexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal.demohexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hexagonal/demohexagonal/DemohexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal.demohexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
